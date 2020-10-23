b76651ea1f313380d7aba3040b8ec78c221a21d7
Socca98 <mihaisol89@yahoo.com>
2020-10-23 19:41:50 +0300

    Added Lab3 - Week4.

---
+++ Lab3/app.py
@@ -0,0 +1,44 @@
+from program import language_specs
+from program.Scanner import Scanner
+
+"""
+Lab3 - Week 4
+"""
+
+files = ['p1err.txt', 'p1.txt', 'p2.txt', 'p3.txt']
+
+scanner = Scanner(files[2])
+scanner.lexical_analysis()
+print(scanner.pif)
+print()
+print(language_specs.codification)
+print("Symbol table: " + str(scanner.st))
+
+
+
+'''
+// Error: constant cannot start with zero
+// Error: invalid identifier name
+// Error: '' not closed.
+
+antete functii date rezultate
+noua diagrama uml
+
+unar - si aritmetic 7--9
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lab3/program/language_specs.py
@@ -0,0 +1,10 @@
+operators = ['+', '-', '*', '/', '=', '<', '>',
+             '<=', '==', '>=', '!=', '&&', '||']
+separators = [';', '{', '}', '(', ')', ' ', '\n', '[', ']', ',']
+reservedWords = ['int', 'char', 'if', 'else', 'while',
+                 'read', 'print', 'list', 'int_main', 'return']
+all_alphabet = operators + separators + reservedWords
+codification = {all_alphabet[i]: i + 2 for i in range(0, len(all_alphabet))}
+codification['identifier'] = 0
+codification['constant'] = 1
+print('Codification: \n', codification)
